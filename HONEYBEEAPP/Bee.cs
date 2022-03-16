24da91d23a6ea1a566a7b6a9da9fc1e20ff8eb10
Vinay Kumar S N <snvinaykumar@outlook.com>
2022-03-16 11:57:58 +0000

    uploading the project

---
+++ HONEYBEEAPP/Bee.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HONEYBEEAPP
+{
+    public class Bee
+    {
+        public float Health { get; protected set; }
+
+        public virtual string BeeType { get; set; }
+        public virtual bool Dead { get; }
+        public Bee()
+        {
+            Health = 100;
+        }
+        public virtual void Damage(int num)
+        {
+            Health = Health - num;
+        }
+
+
+    }
+
+    public class Worker : Bee
+    {
+        public override bool Dead
+        {
+            get
+            {
+                if (Health < 70)
+                    return true;
+                return false;
+            }
+        }
+
+        public override string BeeType
+        {
+            get
+            {
+                return "Worker";
+
+            }
+        }
+        public override void Damage(int num)
+        {
+            if (Health > 70)
+                Health = Health - num;
+        }
+    }
+    public class Queen : Bee
+    {
+        public override bool Dead
+        {
+            get
+            {
+                if (Health < 20)
+                    return true;
+                return false;
+            }
+        }
+        public override string BeeType
+        {
+            get
+            {
+                return "Queen";
+
+            }
+        }
+        public override void Damage(int num)
+        {
+            if (Health > 20)
+                Health = Health - num;
+        }
+    }
+    public class Drone : Bee
+    {
+        public override bool Dead
+        {
+            get
+            {
+                if (Health < 50)
+                    return true;
+                return false;
+            }
+        }
+        public override string BeeType
+        {
+            get
+            {
+                return "Drone";
+
+            }
+        }
+        public override void Damage(int num)
+        {
+            if (Health > 50)
+                Health = Health - num;
+        }
+    }
+}
---
+++ HONEYBEEAPP/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+using HONEYBEEAPP;
+
+List<Bee> BeeList = new List<Bee>();
+
+
+Bee wBee = new Worker();
+Bee qBee = new Queen();
+Bee dBee = new Drone();
+
+bool allDead = false;
+while (!allDead)
+{
+    Random rand = new Random();
+    int randNum = rand.Next(0, 100);
+    wBee.Damage(randNum);
+
+    randNum = rand.Next(0, 100);
+    qBee.Damage(randNum);
+
+    randNum = rand.Next(0, 100);
+    dBee.Damage(randNum);
+
+    if (wBee.Dead && qBee.Dead && dBee.Dead)
+        allDead = true;
+
+    Console.WriteLine("BeeType - " + wBee.BeeType + " BeeHealth - " + wBee.Health + " DeadStatus - " + wBee.Dead);
+    Console.WriteLine("BeeType - " + qBee.BeeType + " BeeHealth - " + qBee.Health + " DeadStatus - " + qBee.Dead);
+    Console.WriteLine("BeeType - " + dBee.BeeType + " BeeHealth - " + dBee.Health + " DeadStatus - " + dBee.Dead);
+
+}
+
+
+
+Console.ReadLine();
